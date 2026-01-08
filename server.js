f3e64e3e2e24a413b2c1c4f6746c2a96645e0819
JEWELJOBY321 <jobysfamily4444@gmail.com>
2026-01-08 18:00:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "PUBLIC")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // MongoDB
 mongoose
@@ -51,11 +51,12 @@ app.get("/admin", async (req, res) => {
 
 // ROOT ROUTE (THIS FIXES NOT FOUND)
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "PUBLIC", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
